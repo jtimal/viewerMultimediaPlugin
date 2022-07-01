a9b1f398f59157725700656708cb32857e123038
Julian Timal <30701057+jtimal@users.noreply.github.com>
2022-07-01 11:17:35 -0500

    viewAudioTemplate

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * @defgroup plugins_generic_viewerMultimedia HTML Article Galley Plugin
+ * @defgroup plugins_generic_mp3ArticleGalley HTML Article Galley Plugin
  */
 
 /**
- * @file plugins/generic/viewerMultimedia/index.php
+ * @file plugins/generic/mp3ArticleGalley/index.php
  *
  * Copyright (c) 2014-2020 Simon Fraser University
  * Copyright (c) 2003-2020 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_generic_viewerMultimedia
+ * @ingroup plugins_generic_mp3ArticleGalley
  * @brief Wrapper for html article galley plugin.
  *
  */
 
-require_once('viewerMultimediaPlugin.inc.php');
+require_once('ViewerMultimediaPlugin.inc.php');
 
-return new viewerMultimediaPlugin();
+return new ViewerMultimediaPlugin();
 
 
